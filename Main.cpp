ec82ef26cc254139f23c3eb387dfc70637f4e702
fruitymario <66334659+aandersen1999@users.noreply.github.com>
2022-01-06 13:05:13 -0500

    pink triangle

--- Main.cpp
+++ Main.cpp
@@ -12,7 +12,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"   FragColor = vec4(0.9f, 0.2f, 0.4f, 1.0f);\n"
 "}\n\0";
 
 int main() 
